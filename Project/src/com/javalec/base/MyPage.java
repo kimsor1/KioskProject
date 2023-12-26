e30d7660848851b8de89191e93c354a24f094419
kimsor1 <imoiksr@gmail.com>
2023-12-26 22:05:21 +0900

    JDialog 추가

---
+++ Project/src/com/javalec/base/MyPage.java
@@ -0,0 +1,36 @@
+package com.javalec.base;
+
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+
+public class MyPage extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyPage dialog = new MyPage();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public MyPage() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
