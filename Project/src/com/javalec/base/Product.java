323d4e62f8d367e9e4762e6e43cdc10c01ab6d4c
kimsor1 <imoiksr@gmail.com>
2023-12-26 22:29:20 +0900

    swing 재생성

---
+++ Project/src/com/javalec/base/Product.java
@@ -0,0 +1,36 @@
+package com.javalec.base;
+
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+
+public class Product extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Product dialog = new Product();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Product() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
