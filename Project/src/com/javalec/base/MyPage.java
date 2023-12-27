3ac9f1aeb550f509c83c6295b5351a46e1d6483f
ilml2 <ilml2@daum.net>
2023-12-27 11:40:17 +0900

    update name.

--- Project/src/com/javalec/base/MyPage.java
+++ Project/src/com/javalec/base/MyPage.java
@@ -3,10 +3,14 @@ package com.javalec.base;
 import java.awt.EventQueue;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class MyPage extends JDialog {
 
 	private static final long serialVersionUID = 1L;
+	private JLabel lblNewLabel;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -29,8 +33,27 @@ public class MyPage extends JDialog {
 	 * Create the dialog.
 	 */
 	public MyPage() {
+		setTitle("My Page");
 		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		getContentPane().add(getLblNewLabel());
+		getContentPane().add(getTextField());
 
 	}
 
+	private JLabel getLblNewLabel() {
+		if (lblNewLabel == null) {
+			lblNewLabel = new JLabel("아이디 :");
+			lblNewLabel.setBounds(36, 21, 61, 16);
+		}
+		return lblNewLabel;
+	}
+	private JTextField getTextField() {
+		if (textField == null) {
+			textField = new JTextField();
+			textField.setBounds(135, 16, 130, 26);
+			textField.setColumns(10);
+		}
+		return textField;
+	}
 }
