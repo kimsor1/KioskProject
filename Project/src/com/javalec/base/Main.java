4b36719dd0da0372eb899e5f2d85baff0e8b1e76
Woody <jsungj3@gmail.com>
2023-12-27 13:14:51 +0900

    Main class with background images

--- Project/src/com/javalec/base/Main.java
+++ Project/src/com/javalec/base/Main.java
@@ -2,11 +2,18 @@ package com.javalec.base;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Main {
 
 	private JFrame frame;
+	private JLabel lbBackgroundImage;
 
 	/**
 	 * Launch the application.
@@ -36,8 +43,36 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setFont(new Font("Dialog", Font.BOLD, 27));
+		frame.setTitle("메인 화면");
+		frame.setBounds(100, 100, 512, 683);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().add(getLbBackgroundImage());
 	}
-
+	private JLabel getLbBackgroundImage() {
+		if (lbBackgroundImage == null) {
+			lbBackgroundImage = new JLabel("");
+			lbBackgroundImage.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					clickMain();
+				}
+			});
+			lbBackgroundImage.setBounds(0, 0, 512, 683);
+			lbBackgroundImage.setIcon(new ImageIcon(Main.class.getResource("/com/javalec/images/MainBack.png")));
+		}
+		return lbBackgroundImage;
+	}
+	
+	// ---- Fucntion ----
+	
+	private void clickMain() {
+		// Main 프레임 오프
+		frame.setVisible(false);
+		
+//		// Login 창 띄우기
+//		Login login = new Login();
+//		login.setVisible(true);
+	}
 }
