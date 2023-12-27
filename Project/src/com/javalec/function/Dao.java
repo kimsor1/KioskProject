626a2b2bcbd57d86f72d814b930c8feaf14a7c93
kimsor1 <imoiksr@gmail.com>
2023-12-27 09:38:13 +0900

    ShareVar 클래스, Dao 클래스 추가

--- Project/src/com/javalec/function/Dao.java
+++ Project/src/com/javalec/function/Dao.java
@@ -1,5 +1,10 @@
 package com.javalec.function;
 
 public class Dao {
+	
+	// Field
+		private final String url_mysql = ShareVar.dbName;
+		private final String id_mysql = ShareVar.dbUser;
+		private final String pass_mysql = ShareVar.dbPass;
 
 }
---
+++ Project/src/com/javalec/function/ShareVar.java
@@ -0,0 +1,10 @@
+package com.javalec.function;
+
+public class ShareVar {
+	
+	public static String dbName = "jdbc:mysql://127.0.0.1:3306/shoekiosk?serverTimezone=UTC&characterEncoding=utf8&userSSL=FALSE";
+	public static String dbUser = "root";
+	public static String dbPass = "qwer1234";
+	public static int filename = 0;
+
+}
