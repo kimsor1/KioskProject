445c7cf1af7df75825ecd4804f2386fe80d863e1
Kanbeomshiek <tvzz1105@naver.com>
2023-12-27 13:16:57 +0900

    purchase 2 차

--- Project/src/com/javalec/base/Purchase.java
+++ Project/src/com/javalec/base/Purchase.java
@@ -3,11 +3,37 @@ package com.javalec.base;
 import java.awt.EventQueue;
 
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.JScrollPane;
+//import javax.swing.JCheckBox;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 public class Purchase extends JDialog {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	private JScrollPane scrollPane;
+	private JTable innerTable;
+	private JButton btnback;
+	private JButton btnbuy;
+	private JButton btndelete;
 
+	
+	private final DefaultTableModel outer_Table = new DefaultTableModel();
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -29,8 +55,151 @@ public class Purchase extends JDialog {
 	 * Create the dialog.
 	 */
 	public Purchase() {
-		setBounds(100, 100, 450, 300);
+		setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 27));
+		setTitle("장바구니 화면");
+		setBounds(100, 100, 512, 683);
+		getContentPane().setLayout(null);
+		getContentPane().add(getTextField());
+		getContentPane().add(getScrollPane());
+		getContentPane().add(getBtnback());
+		getContentPane().add(getBtnbuy());
+		getContentPane().add(getBtndelete());
 
 	}
 
-}
+	private JTextField getTextField() {
+		if (textField == null) {
+			textField = new JTextField();
+			textField.setEditable(false);
+			textField.setHorizontalAlignment(SwingConstants.CENTER);
+			textField.setText("장바구니");
+			textField.setBounds(113, 76, 266, 49);
+			textField.setColumns(10);
+		}
+		return textField;
+	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(66, 198, 389, 211);
+			scrollPane.setViewportView(getInnerTable());
+		}
+		return scrollPane;
+	}
+	private JTable getInnerTable() {
+		if (innerTable == null) {
+			innerTable = new JTable();
+			innerTable.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+									tableInit();
+									
+				}
+			});
+		}
+		return innerTable;
+	}
+	private JButton getBtnback() {
+		if (btnback == null) {
+			btnback = new JButton("뒤로가기");
+			btnback.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+									backAction();
+				}
+			});
+			btnback.setBounds(66, 505, 117, 29);
+		}
+		return btnback;
+	}
+	private JButton getBtnbuy() {
+		if (btnbuy == null) {
+			btnbuy = new JButton("구매확정");
+			btnbuy.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+								buyAction();
+				}
+			});
+			btnbuy.setBounds(338, 505, 117, 29);
+		}
+		return btnbuy;
+	}
+	private JButton getBtndelete() {
+		if (btndelete == null) {
+			btndelete = new JButton("장바구니 제거");
+			btndelete.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+								cartdelete();
+				}
+			});
+			btndelete.setBounds(66, 421, 95, 29);
+		}
+		return btndelete;
+	}
+	
+	
+	//--------------Function------------
+	
+	
+	//장바구니 삭제 기능
+	public void cartdelete() {
+					
+	}
+	
+	
+	
+	//구매창 팝업
+	public void buyAction() {
+		JOptionPane.showMessageDialog(null, "제품을 구매하였습니다.");
+		
+	}
+	
+	
+	//뒤로가기 버튼 활성화
+	public void backAction() {
+		
+	}
+	
+	
+	//innerTable 기능
+	
+
+	public void tableInit() {
+		outer_Table.addColumn("no.");
+		outer_Table.addColumn("제품명");
+		outer_Table.addColumn("색깔");
+		outer_Table.addColumn("사이즈");
+		outer_Table.setColumnCount(4);
+		//순서							
+				int colNo =0;
+				TableColumn col = innerTable.getColumnModel().getColumn(colNo);
+				int width= 20;
+				col.setPreferredWidth(width);
+				//제품명
+				 colNo =1;
+				  col = innerTable.getColumnModel().getColumn(colNo);
+				 width= 150;
+				col.setPreferredWidth(width);
+				//색깔
+				 colNo =2;
+				  col = innerTable.getColumnModel().getColumn(colNo);
+				 width= 50;
+				col.setPreferredWidth(width);
+				//사이즈
+				 colNo =3;
+				  col = innerTable.getColumnModel().getColumn(colNo);
+				 width= 70;
+				col.setPreferredWidth(width);
+				
+//				//초기화 시키기
+//				int i = outer_Table.getRowCount();
+//				for ( int j =0;j<i; j++) {
+//					outer_Table.removeRow(0);
+//				}
+				
+				innerTable.setAutoResizeMode(innerTable.AUTO_RESIZE_OFF);
+				
+	}
+	
+	
+	
+} //End
---
+++ Project/src/com/javalec/function/Dto_Purchase.java
@@ -0,0 +1,44 @@
+package com.javalec.function;
+
+public class Dto_Purchase {
+
+	
+	
+				String pid;
+				String price;
+				String quantity;
+				String date;
+				
+				
+				
+				
+				
+				
+				public String getPid() {
+					return pid;
+				}
+				public void setPid(String pid) {
+					this.pid = pid;
+				}
+				public String getPrice() {
+					return price;
+				}
+				public void setPrice(String price) {
+					this.price = price;
+				}
+				public String getQuantity() {
+					return quantity;
+				}
+				public void setQuantity(String quantity) {
+					this.quantity = quantity;
+				}
+				public String getDate() {
+					return date;
+				}
+				public void setDate(String date) {
+					this.date = date;
+				}
+				
+	
+				
+}
